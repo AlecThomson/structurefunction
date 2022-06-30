46f972a74956d90175a73763c433523fe5bf5bd7
Thomson, Alec (S&A, Kensington) <alec.thomson@csiro.au>
2022-06-30 10:05:45 +0800

    Bump

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/AlecThomson/structurefunction'
 EMAIL = 'alec.thomson@csiro.au'
 AUTHOR = 'Alec Thomson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
